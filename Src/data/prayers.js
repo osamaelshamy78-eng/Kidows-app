36a32380cea4c2c5416ca80344086c6de1bb1bae
osamaelshamy78-eng <osamaelshamy78@gmail.com>
2026-08-20 07:57:59 +0400

    Rename Src directory to src

--- Src/data/prayers.js
+++
@@ -1,7 +0,0 @@
-export const prayers = [
-  { id: 'fajr', name: 'الفجر', rakaat: 2, time: 'بدري قبل ما الدنيا تصبح', icon: '🌅' },
-  { id: 'dhuhr', name: 'الضهر', rakaat: 4, time: 'في نص النهار', icon: '☀️' },
-  { id: 'asr', name: 'العصر', rakaat: 4, time: 'بعد الضهر شوية', icon: '🌤️' },
-  { id: 'maghrib', name: 'المغرب', rakaat: 3, time: 'لما الشمس تغرب', icon: '🌇' },
-  { id: 'isha', name: 'العشاء', rakaat: 4, time: 'بالليل قبل النوم', icon: '🌌' }
-]
