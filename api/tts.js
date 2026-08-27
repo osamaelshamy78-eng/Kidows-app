9f3ca57ad7aa958b1d1d5c09ab892a9597478cf4
osamaelshamy78-eng <osamaelshamy78@gmail.com>
2026-08-28 01:43:48 +0400

    Add files via upload

--- api/tts.js
+++ api/tts.js
@@ -4,7 +4,7 @@
 // it in frontend code.
 // Arabic narration uses the Haytham voice selected for warm, clear storytelling.
 
-const ARABIC_VOICE_ID = 'wxweiHvoC2r2jFM7mS8b'
+const ARABIC_VOICE_ID = 'albaa6OioIhKtKdCEkQw'
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
